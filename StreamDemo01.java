8ba60b3bd7f025e653d64c5465a24ad16a580889
KimTaeYeong <12170548@inha.edu>
2023-02-14 23:51:08 +0900

    day22

---
+++ StreamDemo01.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamDemo01 {
+    public static void main(String[] args) {
+        List<String> cities = Arrays.asList("서울", "부산", "광주", "대구", "부산","서울","대전","서울","경기");
+        //old style
+        Iterator<String> it = cities.iterator();
+        while(it.hasNext()){
+            String city = it.next();
+            System.out.print(city + " ");
+        }
+        System.out.println();
+        //modern style;
+        //Stream<String> st = cities.stream();
+        Stream<String> st = cities.parallelStream();
+        st.forEach(c -> System.out.print(c + " "));
+    }
+}
---
+++ StreamDemo02.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamDemo02 {
+    public static void main(String[] args) {
+        List<String> cities = Arrays.asList("서울", "부산", "광주", "대구", "부산","서울","대전","서울","경기");
+        Stream<String> st = cities.stream();
+        st.filter(city -> city.startsWith("대")).forEach(c -> System.out.print(c + " "));
+        //        st.forEach(c -> System.out.print(c + " "));
+    }
+}
