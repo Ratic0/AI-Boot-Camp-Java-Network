7b88ce4d4ff3e86cb5667db2a9bb0ad10f20fbc3
KimTaeYeong <12170548@inha.edu>
2023-02-14 23:58:05 +0900

    day22_Mission_Thread02

--- day22_Mission_Thread.java
+++ day22_Mission_Thread.java
@@ -1,23 +1,22 @@
-class Worker extends Thread{
-    public void run(){
-        try{
-            for(int i=0; i<5; i++) {
-                System.out.println("작업 스레드 : " + i);
-                Thread.sleep(1000);
-            }
-        } catch (InterruptedException e){}
-    }
-}
+
 public class day22_Mission_Thread {
     public static void main(String[] args) {
         int alphabet = 'a';
-        Worker worker = new Worker();
+        Thread worker = new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("작업 스레드 : " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+            }
+        });
         worker.start();
         try {
-            while (worker.isAlive()) { // boolean isAlive()
-                System.out.println("메인 스레드 :" + (char)alphabet++);
+            while (worker.isAlive()) { //boolean isAlive()
+                System.out.println("메인 스레드 : " + (char) alphabet++);
                 Thread.sleep(500);
             }
-        }catch(InterruptedException e){}
+        } catch (InterruptedException e) { }
     }
 }
