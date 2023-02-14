16a027d3003937b5aae2811e91c54f87e93d7d8e
KimTaeYeong <12170548@inha.edu>
2023-02-15 00:07:27 +0900

    day22_Mission_Thread03

--- day22_Mission_Thread.java
+++ day22_Mission_Thread.java
@@ -1,22 +1,25 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class day22_Mission_Thread {
     public static void main(String[] args) {
         int alphabet = 'a';
-        Thread worker = new Thread(() -> {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Runnable task = () -> {
             try {
                 for (int i = 0; i < 5; i++) {
-                    System.out.println("작업 스레드 : " + i);
+                    System.out.println("작업 스레드 :" + i);
                     Thread.sleep(1000);
                 }
-            } catch (InterruptedException e) {
-            }
-        });
-        worker.start();
+            } catch (InterruptedException e) {}
+            exec.shutdown();
+        };
+        exec.submit(task);
         try {
-            while (worker.isAlive()) { //boolean isAlive()
+            while (exec.isShutdown()!=true) { // 셧다운 이전까지 작동하게
                 System.out.println("메인 스레드 : " + (char) alphabet++);
                 Thread.sleep(500);
             }
-        } catch (InterruptedException e) { }
+        } catch (InterruptedException e) {}
     }
 }
