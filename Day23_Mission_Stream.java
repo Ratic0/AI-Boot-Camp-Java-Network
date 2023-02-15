cdfd800e75ee6909a9236311252e8c86fe397090
KimTaeYeong <12170548@inha.edu>
2023-02-16 00:32:23 +0900

    Day23_Mission_Stream02

--- Day23_Mission_Stream.java
+++ Day23_Mission_Stream.java
@@ -1,23 +1,22 @@
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day23_Mission_Stream {
     public static void main(String[] args) {
-        List<String> names = List.of("홍길동", "배정화", "임꺽정", "연흥부", "김선달", "황진이");
+        enum Gender {남, 여};
         Stream<String> st;
-        st = names.stream();
-        st.filter(f -> f.charAt(0) < '이').forEach(f -> System.out.print(f + " "));
+        List<String> names = List.of("홍길동", "배장화", "임꺽정", "연흥부", "김선달", "황진이");
+        List<Integer> ages = List.of(25,20,29,28,32,18);
+        List<Gender> genders = List.of(Gender.남, Gender.여, Gender.남, Gender.남, Gender.남, Gender.여);
+        Stream<Integer> sti = ages.stream();
+        System.out.println(sti.reduce(Integer::sum).get());
+        sti = ages.stream();
+        System.out.println(sti.max(Integer::compareTo).get());
+        IntStream ss = ages.stream().mapToInt(a -> a.intValue()); // ????
+        System.out.println(ss.average().getAsDouble());
 
-        st = names.stream();
-        st.sorted().forEach(n -> System.out.print(n + " "));
-        System.out.println();
 
-        st = names.stream();
-        System.out.println(st.findFirst());
-        st = names.stream();
-        System.out.println(st.findFirst().get());
-
-        st = names.stream();
-        System.out.println(st.count());
     }
 }
